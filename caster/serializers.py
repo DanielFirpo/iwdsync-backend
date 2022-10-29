0807c6b8c1e287eb0d8dd130c115e781c3f227d3
Brian Perrett <perrettbrian@gmail.com>
2022-10-29 11:23:29 -0600

    Explicit api response.

--- caster/serializers.py
+++ caster/serializers.py
@@ -5,4 +5,12 @@ from caster.models import Caster
 class CasterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Caster
-        fields = '__all__'
+        fields = [
+            'user',
+            'twitch_channel',
+            'url_path',
+            'stream_delay',
+            'youtube_url',
+            'youtube_time',
+            'irl_time'
+        ]
